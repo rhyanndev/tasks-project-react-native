36bdd329781cea8e1cd9491c5e88b469a9d61907
rhyanndev <rhyannjosebezerra@gmail.com>
2024-07-12 15:33:46 -0300

    Encaminhando arquivos para o diretório

---
+++ tasks/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import Navigtor from './src/Navigator';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Navigtor);
